7a001fed2eec68e62a369c54d5b114daec7d95ba
murtaza192168 <murtazacloudwork1252@gmail.com>
2024-08-15 14:37:38 +0530

    Add dynamic PORt address

--- user-management-system/server.js
+++ user-management-system/server.js
@@ -1,13 +1,16 @@
 // import the dependencies
 const express = require('express');
 const axios = require('axios');
-
+const dotenv = require('dotenv')
 // initialising express
 const app = express();
 
+// dotenv.config({path : 'config.env'})
+// initilize he port address using dotenv 
+const PORT = process.env.PORT || 8080
 // requesting a page on the browser by requesting the server...
 app.get('/', (req,res) => {
     res.send("USER MANAGEMENT SYSTEM");
 })
-app.listen(3000, () => {console.log(`Server running on http://localhost:${3000}`);
+app.listen(PORT, () => {console.log(`Server running on http://localhost:${PORT}`);
  })
